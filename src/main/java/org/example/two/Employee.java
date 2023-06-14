c10fce5779f782034ba28c4b1863a678c0b12af3
AlexeyNev <Alex.nevredimoff2011@yandex.ru>
2023-06-14 17:16:01 +0400

    14/06/23

--- src/main/java/org/example/two/Employee.java
+++ src/main/java/org/example/two/Employee.java
@@ -1,4 +1,35 @@
 package org.example.two;
 
 public class Employee {
+    int id;
+    String surname;
+    int age;
+    double salary;
+    String department;
+
+    public Employee(int id, String surname, int age, double salary, String department) {
+        this.id = id;
+        this.surname = surname;
+        this.age = age;
+        this.salary = salary;
+        this.department = department;
+    }
+
+    double salaryTwo() {
+        salary *= 2;
+        return salary;
+    }
+}
+
+class EmployeeTest {
+    public static void main(String[] args) {
+        Employee employee1 = new Employee(1, "Alexeev", 29, 202.5, "IT");
+        Employee employee2 = new Employee(1, "Marsov", 29, 252.5, "Sales");
+
+        employee1.salaryTwo();
+        System.out.println("New zarplata rebotnika " + employee1.surname + ": " + employee1.salary);
+
+        double newSalary = employee2.salaryTwo();
+        System.out.println("New zarplata rabotnika " + employee2.surname + ": " + newSalary);
+    }
 }
