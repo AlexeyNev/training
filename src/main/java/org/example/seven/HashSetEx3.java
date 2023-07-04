1e7d8b52068b11bb73cd217c5430136c8819af73
AlexeyNev <Alex.nevredimoff2011@yandex.ru>
2023-07-04 10:33:02 +0400

    4/07/23 Set HashSet

---
+++ src/main/java/org/example/seven/HashSetEx3.java
@@ -0,0 +1,19 @@
+package org.example.seven;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class HashSetEx3 {
+    public static void main(String[] args) {
+        Set<String> set = new HashSet<>();
+        set.add("Alex");
+        set.add("Alla");
+        set.add("Mars");
+
+//        set.remove("Mars");
+//        System.out.println(set.size());
+//        System.out.println(set.isEmpty());
+//        System.out.println(set.contains("Mar"));
+        
+    }
+}
---
+++ src/main/java/org/example/seven/HashSetEx4.java
@@ -0,0 +1,16 @@
+package org.example.seven;
+
+import java.util.HashSet;
+
+public class HashSetEx4 {
+    public static void main(String[] args) {
+        HashSet<Integer> hashSet = new HashSet<>();
+
+        hashSet.add(5);
+        hashSet.add(2);
+        hashSet.add(3);
+        hashSet.add(1);
+        hashSet.add(8);
+
+    }
+}
