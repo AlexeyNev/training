e194f118cb0ecf7d030d190d7af4bf50316484b0
AlexeyNev <Alex.nevredimoff2011@yandex.ru>
2023-07-13 09:24:40 +0400

    13/07/23 Consumer interface

--- src/main/java/org/example/interface_function/Supplier/Test3.java
+++ src/main/java/org/example/interface_function/Supplier/Test3.java
@@ -2,9 +2,11 @@ package org.example.interface_function.Supplier;
 
 import java.util.ArrayList;
 import java.util.function.Supplier;
+import java.util.function.Consumer;
 
 /**
  * Supplier поставляет объекты
+ * Consumer принимает объекты (в нашем случае изменяем объект с помощью него)
  */
 public class Test3 {
     public static ArrayList<Car> createThreeCars(Supplier<Car> carSupplier) {
@@ -15,8 +17,16 @@ public class Test3 {
         return carArrayList;
     }
 
+    public static void changeCar(Car car, Consumer<Car> carConsumer) {
+        carConsumer.accept(car);
+    }
+
     public static void main(String[] args) {
         ArrayList<Car> ourCars = createThreeCars(() -> new Car("Nissan Tiida", "Silver", 1.6));
+        System.out.println(ourCars);
+
+        changeCar(ourCars.get(0), s -> {s.color = "red"; s.engine = 2.0;
+            System.out.println(s);});
         System.out.println(ourCars);
     }
 }
